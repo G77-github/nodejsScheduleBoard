75698218c875bc661ec7cad75d23c73260f80d04
G77 <lskandar@naver.com>
2023-11-20 14:44:13 +0900

    addLoadscheduleandSavebugfix

--- router/schedule.js
+++ router/schedule.js
@@ -1,35 +1,62 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../configs/db");
+const moment = require("moment");
 
 router.get("/", (req,res)=>{
     res.render("schedule/schedule.ejs");
 });
 
+
 router.post("/", async (req,res)=>{
     const { events } = req.body;
     console.log(events);
-    var sql = "insert into schedules (title, start, end, allDay) values (?, ?, ?, ?)";
+    var insertSql = "insert into schedules (title, start, end, allDay) values (?, ?, ?, ?)";
+    var selectSql = "select * from schedules where title =?";
     try{
         for(let event of events){
-            let start = new Date(event.start).toISOString().slice(0,19).replace('T', ' ');
-            let end = event.end ? new Date(event.end).toISOString().slice(0,19).replace('T', ' ') : null;
+            let start = moment(event.start).format("YYYY-MM-DD HH:mm:ss");
+            let end = event.end ? moment(event.end).format("YYYY-MM-DD HH:mm:ss") : null;
             
-            await new Promise((resolve, reject)=>{
-                db.query(sql,[event.title, start, end, event.allDay], (error, result, fields)=>{
+            let existEvents = await new Promise((resolve, reject)=>{
+                db.query(selectSql, [event.title], (error, results, fields)=>{
                     if(error){
                         reject(error);
                     } else{
-                        resolve();
+                        resolve(results);
                     }
                 });
             });
+
+            if(existEvents.length === 0){
+                await new Promise((resolve, reject)=>{
+                    db.query(sql,[event.title, start, end, event.allDay], (error, result, fields)=>{
+                        if(error){
+                            reject(error);
+                        } else{
+                            resolve();
+                        }
+                    });
+                });
+            }
         }
         res.status(200).send("DB save success");
     } catch(error){
         console.log(error);
         res.status(500).send("DB error");
     }
+});
+
+router.get("/data", (req,res)=>{
+    var sql = "select * from schedules";
+    db.query(sql, (error, results, fields)=>{
+        if(error){
+            console.log(error);
+            res.status(500).send("DB error");
+        } else{
+            res.status(200).json(results);
+        }
+    });
 });
 
 module.exports = router;
