a6fe29ff7febb30064a333cf4b57502f9f3d9fb0
G77 <lskandar@naver.com>
2023-11-24 18:51:36 +0900

    modifyAndDeletechange

--- router/board.js
+++ router/board.js
@@ -12,11 +12,10 @@ router.get("/board/write", (req,res)=>{
 
 router.post("/board/write", (req,res)=>{
     var writer = req.body.writer;
-    var password = req.body.password;
     var title = req.body.title;
     var content = req.body.content;
-    var data = [writer, password, title, content];
-    var sql = "insert into board(id, writer, password, title, content, date) values(null,?,?,?,?,now())";
+    var data = [writer, title, content];
+    var sql = "insert into board(id, writer, title, content, date) values(null,?,?,?,now())";
     db.query(sql, data, (err, result)=>{
         if(err){
             console.error(err);
@@ -35,17 +34,7 @@ router.get("/board", (req,res)=>{
         });
     });
 });
-/*
-router.get("/board/view/:id", (req, res)=>{
-    var id = req.params.id;
-    var sql = "SELECT board.*, boardcomment.* FROM board LEFT JOIN boardcomment ON board.id = boardcomment.bid where id=?;";
-    db.query(sql, [id], (err, rows)=>{
-        res.render("board/detail.ejs", {
-            result: rows
-        });
-    });
-});
-*/
+
 router.get("/board/view/:id", (req, res)=>{
     var id = req.params.id;
     var sql = "SELECT board.*, boardcomment.* FROM board LEFT JOIN boardcomment ON board.id = boardcomment.bid where board.id=?";
@@ -57,14 +46,12 @@ router.get("/board/view/:id", (req, res)=>{
     });
 });
 
-
 router.post("/board/view/:id", (req,res)=>{
     var cwriter = req.body.cwriter;
-    var cpassword = req.body.cpassword;
     var bid = req.params.id;
     var ccontent = req.body.ccontent;
-    var data = [cwriter, cpassword, bid, ccontent, ];
-    var sql = "insert into boardcomment(cid, cwriter, cpassword, bid, ccontent, cdate) value(null,?,?,?,?,now())";
+    var data = [cwriter, bid, ccontent];
+    var sql = "insert into boardcomment(cid, cwriter, bid, ccontent, cdate) value(null,?,?,?,now())";
     db.query(sql, data, (err,result)=>{
         if(err){
             console.error(err);
@@ -75,71 +62,29 @@ router.post("/board/view/:id", (req,res)=>{
     });
 });
 
-router.get("/board/:type/:id", (req,res)=>{
-    var type = req.params.type;
+router.get("/board/modify/:id", (req,res)=>{
     var id = req.params.id;
-
-    if(type == "modify"){
-        var title = "수정";
-    
-    } else if(type == "delete"){
-        var title = "삭제";
-    }
-    res.render("board/auth.ejs",{
-        id: id,
-        type: type,
-        title: title
-    });
-});
-
-router.post("/board/:type/:id", (req,res)=>{
-    var type = req.params.type;
-    var id = req.params.id;
-    var password = req.body.password;
-    var sql = "select password from board where id=?";
-    db.query(sql, [id], (err, rows)=>{
-
+    sql = "select * from board where id =?";
+    db.query(sql, [id],(err, result)=>{
         if(err){
             console.error(err);
             res.sendStatus(500);
-            return;
-        }
-
-
-        var temp = rows[0].password;
-        if(temp != password){
-            res.render("board/error");
-
         } else{
-            if(type == "modify"){
-                var sql = "select * from board where id=?";
-                db.query(sql, [id], (err, rows)=>{
-                    res.render("board/modify",{
-                        head: "글 수정하기",
-                        id: id,
-                        writer: rows[0].writer,
-                        title: rows[0].title,
-                        content: rows[0].content
-                    });
-                });
-            } else if(type == "delete"){
-                var sql ="delete from board where id=?";
-                db.query(sql,[id]);
-                res.redirect("/tab/about");
-            }
+            res.render("board/modify.ejs", {
+                result: result[0],
+                username: req.session.username
+            });
         }
     });
 });
 
 router.post("/board/modify/:id/ok", (req,res)=>{
     var id = req.params.id;
-    var password = req.body.password;
-    var writer = req.body.writer;
     var title = req.body.title;
     var content = req.body.content;
     
-    var sql = "update board set password=?, writer=?, title=?, content=? where id=?";
-    var data = [password, writer, title, content, id];
+    var sql = "update board set title=?, content=? where id=?";
+    var data = [title, content, id];
 
     db.query(sql, data, (err, result)=>{
         if(err){
@@ -150,5 +95,20 @@ router.post("/board/modify/:id/ok", (req,res)=>{
         }
     });
 });
+
+router.post("/board/delete", (req,res)=>{
+    var id = req.body.id;
+    var sql = "delete from board where id =?";
+
+    db.query(sql, [id], (err)=>{
+        if(err){
+            console.error(err);
+            res.sendStatus(500);
+        } else{
+            res.redirect("/tab/about");
+        }
+    });
+});
+
 
 module.exports = router;
